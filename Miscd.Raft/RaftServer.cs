ca8e01197cba019fb576fbe0aca7573b89889a79
Dylan Sprague <dylan.richard.sprague@gmail.com>
2020-07-15 03:57:00 -0400

    Wire up events to event handlers for RaftServer

--- Miscd.Raft/RaftServer.cs
+++ Miscd.Raft/RaftServer.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.Coyote.Actors;
+﻿using Microsoft.Coyote;
+using Microsoft.Coyote.Actors;
+using Miscd.Raft.Events;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Miscd.Raft
 {
@@ -44,11 +45,75 @@ namespace Miscd.Raft
         #region State machine states
 
         [Start]
+        [OnEntry(nameof(BecomeFollower))]
+        [OnEventDoAction(typeof(VoteRequestEvent), nameof(RespondToVoteRequest))]
+        [OnEventDoAction(typeof(VoteResponseEvent), nameof(UpdateLocalState))]
+        [OnEventDoAction(typeof(AppendEntriesRequestEvent), nameof(AcceptEntriesAsFollower))]
+        [OnEventDoAction(typeof(AppendEntriesResponseEvent), nameof(UpdateLocalState))]
         private class Follower : State { }
         
+        [OnEntry(nameof(BecomeCandidate))]
+        [OnEventDoAction(typeof(VoteRequestEvent), nameof(RespondToVoteRequest))]
+        [OnEventDoAction(typeof(VoteResponseEvent), nameof(AcceptVoteResponse))]
+        [OnEventDoAction(typeof(AppendEntriesRequestEvent), nameof(AcceptEntriesAsCandidate))]
+        [OnEventDoAction(typeof(AppendEntriesResponseEvent), nameof(UpdateLocalState))]
         private class Candidate : State { }
         
+        [OnEntry(nameof(BecomeLeader))]
+        [OnEventDoAction(typeof(VoteRequestEvent), nameof(RespondToVoteRequest))]
+        [OnEventDoAction(typeof(VoteResponseEvent), nameof(UpdateLocalState))]
+        [OnEventDoAction(typeof(AppendEntriesRequestEvent), nameof(UpdateLocalState))]
+        [OnEventDoAction(typeof(AppendEntriesResponseEvent), nameof(AcceptAppendEntriesResponse))]
         private class Leader : State { }
+
+        #endregion
+
+        #region Event handlers
+
+        private void BecomeFollower(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BecomeCandidate(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BecomeLeader(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RespondToVoteRequest(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AcceptVoteResponse(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateLocalState(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AcceptEntriesAsFollower(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AcceptEntriesAsCandidate(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AcceptAppendEntriesResponse(Event e)
+        {
+            throw new NotImplementedException();
+        }
 
         #endregion
     }
