fb7c21c3dba542025b36ea06b4dd9eeb803d8b12
Dylan Sprague <dsprague@docuphase.com>
2020-07-15 20:23:43 -0400

    Implement ElectionSafety spec

--- Miscd.Raft.Tests/Specifications/ElectionSafety.cs
+++ Miscd.Raft.Tests/Specifications/ElectionSafety.cs
@@ -1,9 +1,42 @@
-﻿using Microsoft.Coyote.Specifications;
+﻿using Microsoft.Coyote;
+using Microsoft.Coyote.Specifications;
+using Miscd.Raft.Events;
+using Miscd.Raft.Events.DiagnosticEvents;
+using System.Collections.Generic;
 
 namespace Miscd.Raft.Tests.Specifications
 {
     // At most one leader can be elected in a given term
     public class ElectionSafety : Monitor
     {
+        private Dictionary<Term, RaftServerId> Elections { get; } = new Dictionary<Term, RaftServerId>();
+
+        [Start]
+        [OnEventDoAction(typeof(LeaderElectedEvent), nameof(RecordLeaderElection))]
+        [IgnoreEvents(
+            typeof(AppendEntriesRequestEvent),
+            typeof(AppendEntriesResponseEvent),
+            typeof(ClientRequestEvent),
+            typeof(VoteRequestEvent),
+            typeof(VoteResponseEvent),
+            typeof(LogEntryAppliedEvent)
+        )]
+        private class Monitoring : State
+        {
+        }
+
+        private void RecordLeaderElection(Event e)
+        {
+            var election = e as LeaderElectedEvent;
+
+            if (Elections.ContainsKey(election.Term) && !Elections[election.Term].Equals(election.LeaderId))    // TODO convert to !=
+            {
+                Assert(false);
+            }
+            else
+            {
+                Elections[election.Term] = election.LeaderId;
+            }
+        }
     }
 }
