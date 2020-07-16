4d4c6af70a27e7961f44380d1d25b49da7c37a2a
Dylan Sprague <dsprague@docuphase.com>
2020-07-15 20:32:04 -0400

    Begin building out LeaderCompleteness spec

--- Miscd.Raft.Tests/Specifications/LeaderCompleteness.cs
+++ Miscd.Raft.Tests/Specifications/LeaderCompleteness.cs
@@ -1,9 +1,35 @@
-﻿using Microsoft.Coyote.Specifications;
+﻿using Microsoft.Coyote;
+using Microsoft.Coyote.Specifications;
+using Miscd.Raft.Events;
+using Miscd.Raft.Events.DiagnosticEvents;
 
 namespace Miscd.Raft.Tests.Specifications
 {
     // If a log entry is committed in a given term, then that entry will be present in the logs of the leaders for all higher-numbered terms.
     public class LeaderCompleteness : Monitor
     {
+        [Start]
+        [OnEventDoAction(typeof(LeaderElectedEvent), nameof(RecordLeaderElection))]
+        [OnEventDoAction(typeof(LogEntryAppliedEvent), nameof(RecordLogEntryApplication))]
+        [IgnoreEvents(
+            typeof(AppendEntriesRequestEvent),
+            typeof(AppendEntriesResponseEvent),
+            typeof(ClientRequestEvent),
+            typeof(VoteRequestEvent),
+            typeof(VoteResponseEvent)
+        )]
+        private class Monitoring : State
+        {
+        }
+
+        private void RecordLeaderElection(Event e)
+        {
+            var election = e as LeaderElectedEvent;
+        }
+
+        private void RecordLogEntryApplication(Event e)
+        {
+            var logEntryApplication = e as LogEntryAppliedEvent;
+        }
     }
 }
