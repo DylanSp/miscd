7ccf0fd0823b894913df46905fab8c0c5a25a60d
Dylan Sprague <dsprague@docuphase.com>
2020-07-15 20:23:28 -0400

    Add diagnostic events for safety checks to monitor

---
+++ Miscd.Raft/Events/DiagnosticEvents/LeaderElectedEvent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Coyote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miscd.Raft.Events.DiagnosticEvents
+{
+    public class LeaderElectedEvent : Event
+    {
+        public Term Term { get; }
+        public RaftServerId LeaderId { get; }
+
+        public LeaderElectedEvent(Term term, RaftServerId leaderId)
+        {
+            Term = term;
+            LeaderId = leaderId;
+        }
+    }
+}
---
+++ Miscd.Raft/Events/DiagnosticEvents/LogEntryAppliedEvent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Coyote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miscd.Raft.Events.DiagnosticEvents
+{
+    public class LogEntryAppliedEvent : Event
+    {
+        public LogEntry Entry { get; }
+
+        public Term Term { get; }
+
+        public LogEntryAppliedEvent(LogEntry entry, Term term)
+        {
+            Entry = entry;
+            Term = term;
+        }
+    }
+}
